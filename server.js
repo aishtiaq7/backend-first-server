68eb23567c2d6b673625d39aa9e2fd7a0ce2814c
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-19 01:26:45 +0600

    switching up server.js file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
+console.log('----here----!_!_!')
 app.get('/', function (req, res) {
+  console.log('inside - / funcion_!_!_!')
   res.send('Hello World')
 })
 
